bf52e675b5256a654eca6e5f2b32f42e4d33d834
Steven T <steventso92@gmail.com>
2013-02-09 18:03:02 -0500

    Basic Spreadsheet Functionality
    Needs some work still with size and positioning

--- wxwidget.py
+++ wxwidget.py
@@ -1,18 +1,68 @@
 import os
+from wx.lib import sheet
 import wx
 
-wildcard = "Python source (*.py)|*.py|" \
+wildcard = "Python source (*.txt)|*.txt|" \
             "All files (*.*)|*.*"
+num_lines = 0;
+ACCEL_X = []
+ACCEL_Y = []
+ACCEL_Z = []
+GYRO_X = []
+GYRO_Y = []
+GYRO_Z = []
+# some constants
+samp_rate = 20
+sim_time = 60
+nsamps = samp_rate*sim_time
+cuttoff_freq = 0.1
+
+
+class parser():
+    global num_lines
+    num_lines= sum(1 for line in open('DATA.TXT', 'r'))
+    f = open('data.txt', 'r')
+    for var0 in range(0, num_lines):
+        line = f.readline();
+        #removes last comma and new line
+        parsed_line = line[:-2];
+        
+        parsed_line = parsed_line.split(',');
+        
+        for var1 in range(0, 6):
+            #removes white spaces
+            if parsed_line[var1][-1:] == ' ':
+                parsed_line[var1] = parsed_line[var1].rstrip();
+            #remove escape character
+            if parsed_line[var1][-1:] == '\x00':
+                parsed_line[var1] = parsed_line[var1].rstrip('\x00');
+            #parsed_line from string ---> int format
+            parsed_line[var1] = int(parsed_line[var1])
+
+        ACCEL_X.append(parsed_line[0]);
+        ACCEL_Y.append(parsed_line[1]);
+        ACCEL_Z.append(parsed_line[2]);
+        GYRO_X.append(parsed_line[3]);
+        GYRO_Y.append(parsed_line[4]);
+        GYRO_Z.append(parsed_line[5]);
+
+
+class DataInput(sheet.CSheet):
+    def __init__(self, parent):
+        sheet.CSheet.__init__(self, parent)
+        self.row = self.col = 0
+        self.SetNumberRows(num_lines)
+        self.SetNumberCols(6)
 
 class Dragon(wx.Frame):
 
     def __init__(self, *args, **kwargs):
-        super(Dragon, self).__init__(*args, **kwargs) 
+        super(Dragon, self).__init__(*args, **kwargs)
         self.currentDirectory = os.getcwd()
         self.InitUI()
                 
     def InitUI(self):
-        self.SetSize((300, 200))
+        self.SetSize((600, 400))
         self.SetTitle('DF Smart Data')
         self.Centre()
 
@@ -56,10 +106,30 @@ class Dragon(wx.Frame):
 
 
         #---IMAGE---#
-        image = 'Icons/article32.png'
-        img = wx.Image(image, wx.BITMAP_TYPE_ANY)
-    	self.sBmp = wx.StaticBitmap(self, wx.ID_ANY, wx.BitmapFromImage(img))
+        #image = 'Icons/article32.png'
+        #img = wx.Image(image, wx.BITMAP_TYPE_ANY)
+    	#self.sBmp = wx.StaticBitmap(self, wx.ID_ANY, wx.BitmapFromImage(img))
 
+        #---FRAME---#
+        box1 = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(box1)
+        notebook1 = wx.Notebook(self, -1, style=wx.BOTTOM)
+
+        #parser()
+
+        sheet1 = DataInput(notebook1)
+        sheet1.SetFocus()
+        notebook1.AddPage(sheet1, 'Original')
+        box1.Add(notebook1, 1, wx.EXPAND)
+
+
+        box2 = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(box2)
+        notebook2 = wx.Notebook(self, -1, style=wx.BOTTOM)
+        sheet2 = DataInput(notebook2)
+        #sheet2.SetFocus()
+        notebook2.AddPage(sheet2, 'Modified')
+        box2.Add(notebook2, 1, wx.EXPAND)
 
         self.Show(True)
     
@@ -75,7 +145,8 @@ class Dragon(wx.Frame):
             paths = dlg.GetPaths()
             print "You chose the following file(s):"
             for path in paths:
-                print path
+                parser()
+
         dlg.Destroy()
 
     def OnSave(self, e):
@@ -99,8 +170,8 @@ class Dragon(wx.Frame):
     def OnAbout(self, e):
         self.Close()
 
+
 def main():
-    
     df = wx.App()
     Dragon(None)
     df.MainLoop()    
