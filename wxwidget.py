dfd796f240bdacf6b8a66274a8a75e6e0f073733
Steven T <steventso92@gmail.com>
2013-02-10 03:14:41 -0500

    Variable file path parsing support
    Does not refresh screen and will have to switch from spreadsheet to
    regular text

--- wxwidget.py
+++ wxwidget.py
@@ -20,13 +20,45 @@ cuttoff_freq = 0.1
 
 ID_BUTTON=100
 
+class parser:
+    def __init__(self, path):
+        #for filePath in path:
+        #   stringPath.append(filePath)
+        stringPath = ''.join(path)
+
+        num_lines= sum(1 for line in open(stringPath, 'r'))
+        f = open(stringPath, 'r')
+        for var0 in range(0, num_lines):
+            line = f.readline();
+            #removes last comma and new line
+            parsed_line = line[:-2];
+            
+            parsed_line = parsed_line.split(',');
+            
+            for var1 in range(0, 6):
+                #removes white spaces
+                if parsed_line[var1][-1:] == ' ':
+                    parsed_line[var1] = parsed_line[var1].rstrip();
+                #remove escape character
+                if parsed_line[var1][-1:] == '\x00':
+                    parsed_line[var1] = parsed_line[var1].rstrip('\x00');
+                #parsed_line from string ---> int format
+                parsed_line[var1] = int(parsed_line[var1])
+
+            ACCEL_X.append(parsed_line[0]);
+            ACCEL_Y.append(parsed_line[1]);
+            ACCEL_Z.append(parsed_line[2]);
+            GYRO_X.append(parsed_line[3]);
+            GYRO_Y.append(parsed_line[4]);
+            GYRO_Z.append(parsed_line[5]);
+
 class Dragon(wx.Frame):
 
     def __init__(self, *args, **kwargs):
         super(Dragon, self).__init__(*args, **kwargs)
         self.currentDirectory = os.getcwd()
-        self.InitUI()
-                
+        self.InitUI()   
+
     def InitUI(self):
         self.SetSize((1200, 650))
         self.SetTitle('DF Smart Data')
@@ -139,10 +171,8 @@ class Dragon(wx.Frame):
             )
         if dlg.ShowModal() == wx.ID_OK:
             paths = dlg.GetPaths()
-            print "You chose the following file(s):"
-            for path in paths:
-                print "Test"
-                #parser()
+            #for path in paths:
+            parser(paths)
 
         dlg.Destroy()
 
@@ -168,34 +198,7 @@ class Dragon(wx.Frame):
         hyper1 = hl.HyperLinkCtrl(self.bottompanel, -1, "Basic Electronixs", pos=(100, 250), URL="http://www.BasicElectronixs.com/")
         self.Close()
 
-    def parser():
-        global num_lines
-        print "ENTERED"
-        num_lines= sum(1 for line in open('DATA.TXT', 'r'))
-        f = open('data.txt', 'r')
-        for var0 in range(0, num_lines):
-            line = f.readline();
-            #removes last comma and new line
-            parsed_line = line[:-2];
-            
-            parsed_line = parsed_line.split(',');
-            
-            for var1 in range(0, 6):
-                #removes white spaces
-                if parsed_line[var1][-1:] == ' ':
-                    parsed_line[var1] = parsed_line[var1].rstrip();
-                #remove escape character
-                if parsed_line[var1][-1:] == '\x00':
-                    parsed_line[var1] = parsed_line[var1].rstrip('\x00');
-                #parsed_line from string ---> int format
-                parsed_line[var1] = int(parsed_line[var1])
 
-            ACCEL_X.append(parsed_line[0]);
-            ACCEL_Y.append(parsed_line[1]);
-            ACCEL_Z.append(parsed_line[2]);
-            GYRO_X.append(parsed_line[3]);
-            GYRO_Y.append(parsed_line[4]);
-            GYRO_Z.append(parsed_line[5]);
 
 def main():
     df = wx.App()
