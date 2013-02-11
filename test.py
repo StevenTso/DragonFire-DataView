8ed4873c062a332215c25883a37d57a7a92cd0e3
Steven T <steventso92@gmail.com>
2013-02-11 01:26:27 -0500

    Input file support
    Can now import file and display text to left panel

--- test.py
+++ test.py
@@ -1,27 +1,48 @@
+# Get the GUI stuff
 import wx
 
-class MainFrame(wx.Frame):
+# We're going to be handling files and directories
+import os
 
-    def __init__(self,parent,id,title,position,size):
-        wx.Frame.__init__(self, parent, id, title, position, size)
-        toolbar = self.CreateToolBar()
-        tool = toolbar.AddLabelTool(wx.ID_ANY, 'TEST', wx.Bitmap('Icons/folder32.png')) #open
-        toolbar.Realize()
-        tool = toolbar.AddLabelTool(wx.ID_ANY, 'TEST2', wx.Bitmap('Icons/arrowright32.png')) #generate
-        toolbar.Realize()
-        self.split1 = wx.SplitterWindow(self)
-        self.split2 = wx.SplitterWindow(self.split1)
+# Set up some button numbers for the menu
 
-        self.bottompanel = wx.Panel(self.split1)
-        self.topleftpanel = wx.Panel(self.split2)
-        self.toprightpanel = wx.Panel(self.split2)
-        self.topleftpanel.SetBackgroundColour('sky Blue')
+ID_ABOUT=101
+ID_OPEN=102
+ID_SAVE=103
+ID_BUTTON1=300
+ID_EXIT=200
 
-        self.split1.SplitHorizontally(self.split2, self.bottompanel)
-        self.split2.SplitVertically(self.topleftpanel, self.toprightpanel)
+class MainWindow(wx.Frame):
+    def __init__(self,parent,title):
+        # based on a frame, so set up the frame
+        wx.Frame.__init__(self,parent,wx.ID_ANY, title)
+
+        # Add a text editor and a status bar
+        # Each of these is within the current instance
+        # so that we can refer to them later.
+        self.control = wx.TextCtrl(self, 1, style=wx.TE_MULTILINE)
+
+        # Setting up the menu. filemenu is a local variable at this stage.
 
 
-app = wx.App(0)
-win = MainFrame(None, -1, "Hello!", (50, 50), (600, 400))
-win.Show()
+        # Set up the overall frame verically - text edit window above buttons
+        # We want to arrange the buttons vertically below the text edit window
+        self.sizer=wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.control,1,wx.EXPAND)
+
+        # Tell it which sizer is to be used for main frame
+        # It may lay out automatically and be altered to fit window
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(1)
+        self.sizer.Fit(self)
+
+        # Show it !!!
+        self.Show(1)
+
+
+
+# Set up a window based app, and create a main window in it
+app = wx.PySimpleApp()
+view = MainWindow(None, "Sample editor")
+# Enter event loop
 app.MainLoop()
--- wxwidget.py
+++ wxwidget.py
@@ -12,6 +12,7 @@ ACCEL_Z = []
 GYRO_X = []
 GYRO_Y = []
 GYRO_Z = []
+FORMATTED_DATA = []
 # some constants
 samp_rate = 20
 sim_time = 60
@@ -20,13 +21,15 @@ cuttoff_freq = 0.1
 
 ID_BUTTON=100
 
-class parser:
+class graphParser:
     def __init__(self, path):
         #for filePath in path:
         #   stringPath.append(filePath)
         stringPath = ''.join(path)
 
-        num_lines= sum(1 for line in open(stringPath, 'r'))
+        global num_lines
+        num_lines = sum(1 for line in open(stringPath, 'r'))
+
         f = open(stringPath, 'r')
         for var0 in range(0, num_lines):
             line = f.readline();
@@ -45,6 +48,7 @@ class parser:
                 #parsed_line from string ---> int format
                 parsed_line[var1] = int(parsed_line[var1])
 
+            global ACCEL_X, ACCEL_Y, ACCEL_Z, GYRO_X, GYRO_Y, GYRO_Z    
             ACCEL_X.append(parsed_line[0]);
             ACCEL_Y.append(parsed_line[1]);
             ACCEL_Z.append(parsed_line[2]);
@@ -52,6 +56,21 @@ class parser:
             GYRO_Y.append(parsed_line[4]);
             GYRO_Z.append(parsed_line[5]);
 
+class viewParser:
+    def __init__(self, path):
+        global FORMATTED_DATA
+        FORMATTED_DATA = []
+        graphParser(path)
+        for var in range(0, num_lines):
+            ax = str(ACCEL_X[var]).zfill(5) + "    |    "
+            ay = str(ACCEL_X[var]).zfill(5) + "    |    "
+            az = str(ACCEL_X[var]).zfill(5) + "    |    "
+            gx = str(ACCEL_X[var]).zfill(5) + "    |    "
+            gy = str(ACCEL_X[var]).zfill(5) + "    |    "
+            gz = str(ACCEL_X[var]).zfill(5) + "   |"
+            newLine = ax + ay + az + gx + gy + gz
+            FORMATTED_DATA.append(newLine)
+            
 class Dragon(wx.Frame):
 
     def __init__(self, *args, **kwargs):
@@ -107,23 +126,19 @@ class Dragon(wx.Frame):
         self.split1 = wx.SplitterWindow(self)
         self.split2 = wx.SplitterWindow(self.split1)
 
-        #---SPREADSHEET---#
+        #---TEXT FRAMES---#
         self.topleftpanel = wx.Panel(self.split2)
         self.toprightpanel = wx.Panel(self.split2)
 
-        self.gridL = gridlib.Grid(self.topleftpanel)
-        self.gridL.CreateGrid(num_lines,6)
+        self.textL = wx.TextCtrl(self.topleftpanel, 1, style=wx.TE_MULTILINE)
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.gridL, 1, wx.EXPAND, 5)
+        sizer.Add(self.textL, 1, wx.EXPAND, 3)
         self.topleftpanel.SetSizer(sizer)
 
-
-        self.gridR = gridlib.Grid(self.toprightpanel)
-        self.gridR.CreateGrid(num_lines,6)
+        self.textR = wx.TextCtrl(self.toprightpanel, 1, style=wx.TE_MULTILINE)
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.gridR, 1, wx.EXPAND, 5)
+        sizer.Add(self.textR, 1, wx.EXPAND, 4)
         self.toprightpanel.SetSizer(sizer)
-
 
 
         #---BOTTOM PANEL---#   
@@ -172,7 +187,12 @@ class Dragon(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             paths = dlg.GetPaths()
             #for path in paths:
-            parser(paths)
+            graphParser(paths)
+            viewParser(paths)
+            self.textL.AppendText("ACCEL_X |  ACCEL_Y  |  ACCEL_Z   |   GYRO_X  |   GYRO_Y   |   GYRO_Z  |\n")
+            for i in range(0, num_lines):
+                self.textL.AppendText(FORMATTED_DATA[i])
+                self.textL.AppendText('\n')
 
         dlg.Destroy()
 
